ae34eb595e6caf9890caef7b58b77a7a1b583bae
Johnstx <johnukiwe@gmail.com>
2025-08-06 17:11:36 +0100

    eks-cluster-new

--- App/fundwave-eks-terraform/versions.tf
+++ App/fundwave-eks-terraform/versions.tf
@@ -1,5 +1,16 @@
+# terraform {
+#   required_version = ">= 1.6.6"
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 6.0"
+#     }
+#   }
+# }
+
 terraform {
-  required_version = ">= 1.6.6"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
