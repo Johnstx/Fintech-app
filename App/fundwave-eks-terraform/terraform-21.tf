3602dccc1e648656113b5f28a76bc985922c416c
Johnstx <johnukiwe@gmail.com>
2025-08-14 01:16:47 +0100

    az-update

--- App/fundwave-eks-terraform/terraform-21.tf
+++ App/fundwave-eks-terraform/terraform-21.tf
@@ -29,7 +29,7 @@ locals {
   cluster_version = "1.33"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
   tags = {
     Example    = local.name
